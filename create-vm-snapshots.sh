98d8b317d750d2d32888d1926309b9b2f5adf79f
Mike Pennisi <mike@mikepennisi.com>
2022-03-16 15:14:06 -0400

    Create VM snapshots

---
+++ create-vm-snapshots.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+if vagrant snapshot list | grep root &> /dev/null ; then
+  echo "Snapshot root already exists"
+else
+  echo "Creating VM from scratch"
+  vagrant destroy -f
+  vagrant up
+  vagrant halt
+  vagrant snapshot save root
+fi
+
+if vagrant snapshot list | grep noscreenreader &> /dev/null ; then
+  echo "Snapshot noscreenreader already exists"
+else
+  echo "Creating noscreenreader snapshot"
+  vagrant halt -f
+  vagrant snapshot restore --no-start root
+  vagrant up --provision-with start-assistive-playwright,start-selenium-webdriver
+  VBoxManage controlvm win10-chromium-nvda setvideomodehint 1920 1080 32
+  vagrant snapshot save noscreenreader
+  vagrant halt -f
+fi
+
+if vagrant snapshot list | grep nvda &> /dev/null ; then
+  echo "Snapshot nvda already exists"
+else
+  echo "Creating nvda snapshot"
+  vagrant halt -f
+  vagrant snapshot restore --no-start noscreenreader
+  vagrant up --provision-with start-nvda
+  vagrant snapshot save nvda
+  vagrant halt -f
+fi
