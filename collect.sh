7d5a50e815cf679e4960fb7f4865f41fd619281e
Mike Pennisi <mike@mikepennisi.com>
2022-03-16 12:00:01 -0400

    Document hard-coded IP address

--- collect.sh
+++ collect.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# > Even though the NAT engine separates the VM from the host, the VM has
+# > access to the host's loopback interface and the network services running on
+# > it. The host's loopback interface is accessible as IP address 10.0.2.2.
+host_machine_loopback=10.0.2.2
+
 # > The Oracle VM VirtualBox web service, vboxwebsrv, is used for controlling
 # > Oracle VM VirtualBox remotely.
 #
@@ -18,7 +23,7 @@ find aria-at/build/tests/ -mindepth 1 -maxdepth 1 -type d -print0 |
   while IFS= read -r -d '' directory; do
     ./aria-at-automation-harness/bin/host.js \
       run-plan \
-      --reference-hostname 10.0.2.2 \
+      --reference-hostname ${host_machine_loopback} \
       --debug \
       --plan-workingdir ${directory} \
       --tests-match '*nvda.collected.json' \
