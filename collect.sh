0a71f210bf186cb68ba84d3ff838a7bb0929ae0d
Mike Pennisi <mike@mikepennisi.com>
2022-03-16 11:56:49 -0400

    Run VirtualBox web server

--- collect.sh
+++ collect.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# > The Oracle VM VirtualBox web service, vboxwebsrv, is used for controlling
+# > Oracle VM VirtualBox remotely.
+#
+# https://docs.oracle.com/en/virtualization/virtualbox/6.0/admin/vboxwebsrv-daemon.html
+vboxwebsrv --authentication null &
+
+virtualbox_server_pid=$!
+
+function kill_virtualbox_server {
+  kill -9 ${virtualbox_server_pid}
+}
+
+trap kill_virtualbox_server EXIT
+
 find aria-at/build/tests/ -mindepth 1 -maxdepth 1 -type d -print0 |
   while IFS= read -r -d '' directory; do
     ./aria-at-automation-harness/bin/host.js \
